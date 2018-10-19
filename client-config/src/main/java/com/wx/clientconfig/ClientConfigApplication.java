74c6b0ab48569f6bc10efc03d525fd5aaa659308
WangAlainDelon <957038053@qq.com>
2018-10-19 10:37:18 +0800

    消息总线刷新配置

---
+++ client-config/src/main/java/com/wx/clientconfig/ClientConfigApplication.java
@@ -0,0 +1,27 @@
+package com.wx.clientconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+@RefreshScope
+public class ClientConfigApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ClientConfigApplication.class, args);
+    }
+    @Value("${java}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
